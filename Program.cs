e3487069c2158bbbd311d536c722ad970f698d0e
rakesh2k1032 <34396156+rakesh2k1032@users.noreply.github.com>
2018-04-09 17:00:40 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Delegates
+{
+    public delegate void DelCall(string s);
+    class Program
+    {
+        public static void returnDel(string s) 
+        {
+            Console.WriteLine("Delegate invoked");
+            Console.ReadLine();
+        }
+        static void Main(string[] args)
+        {
+            //DelCall d = new DelCall(returnDel);
+            //d.Invoke("rakesh");
+
+            DelCall de = rajesh => {  };
+            //de.Invoke("ok");
+        }
+    }
+}
