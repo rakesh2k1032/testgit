dbc09a15c51e037f8892300b2d73043daef88197
rakesh2k1032 <34396156+rakesh2k1032@users.noreply.github.com>
2018-04-10 10:26:04 +0530

    Morning 10:25

---
+++ Product.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Collections.ObjectModel;
+
+namespace DragAndDrop
+{
+    public class Product
+    {
+        public string Name { get; set; }
+        public int Qty { get; set; }
+        public string Description { get; set; }
+
+        public static ObservableCollection<Product> GetProducts()
+        {
+            return new ObservableCollection<Product>(new Product[] {
+                    new Product { Name="Product1", Qty=10, Description="Description 1" },
+                    new Product { Name="Product2", Qty=5, Description="Description 2" },
+                    new Product { Name="Product3", Qty=15, Description="Description 3" }
+            });
+        }
+    }
+}
